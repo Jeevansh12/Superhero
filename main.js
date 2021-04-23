3e571355f86e80364aa3b3aef9450355d0760724
Jeevansh12 <76777313+Jeevansh12@users.noreply.github.com>
2021-04-23 17:36:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,4 +16,85 @@ player_object.set({
 });
 canvas.add(player_object);
     });
+}function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object = Img;
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.set(
+            {
+                top:player_y,
+                left:player_x
+            }
+        );
+        canvas.add(block_image_object);
+    
+        
+    });
 }
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    if(e.shiftKey == true && keyPressed=='80'){
+        console.log("p&shift pressed together");
+        block_image_width = block_image_width+10;
+        block_image_height = block_image_height+10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if(e.shiftKey == true && keyPressed=='77'){
+        console.log("m&shift pressed together");
+        block_image_width = block_image_width-10;
+        block_image_height = block_image_height-10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if(keyPressed=='38'){
+        up();
+        console.log("up");
+
+    }
+    if(keyPressed=='40'){
+        down();
+        console.log("down");
+        
+    }
+    if(keyPressed=='37'){
+        left();
+        console.log("left");
+        
+    }
+    if(keyPressed=='39'){
+        right();
+        console.log("right");
+        
+    }
+    if(keyPressed == '70' ){
+        new_image("ironman_face.png");
+        console.log("f");
+        
+    }
+    if(keyPressed == '66' ){
+        new_image("spiderman_body.png");
+        console.log("b");
+        
+    }
+    if(keyPressed == '76' ){
+        new_image("hulk_legs.png");
+        console.log("l");
+        
+    }
+    if(keyPressed =='82'){
+        new_image("thor_right_hand.png");
+        console.log("r");  
+        
+    }
+    
+    if(keyPressed =='65'){
+        new_image("captain_america_left_hand.png");
+        console.log("a");  
+        
+    }
+        
+    }
+
